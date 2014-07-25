2639f05890ae8d5b91ba9965a374813d0a40e1f1
Romain Francois <romain@r-enthusiasts.com>
2014-07-26 00:12:12 +0200

    just cosmetics and indenting

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,18 @@
 .onAttach <- function(libname, pkgname) {
-  # Don't unload self when being installed - that will result in install error
-  if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
-    try( unloadNamespace("nothing"), silent = TRUE )
-  }
+    # Don't unload self when being installed - that will result in install error
+    if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
+        try( unloadNamespace("nothing"), silent = TRUE )
+    }
 }
    
 .onLoad <- function(libname, pkgname){
     if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
-        pkgs <- setdiff(loadedNamespaces(), "base")  
-        while (length(pkgs) > 0) {
-          for (pkg in pkgs) {
-            tryCatch(unloadNamespace(pkg), error = function (e) invisible())
-          }
-        
-          pkgs <- setdiff(loadedNamespaces(), "base")
+        repeat{
+            pkgs <- setdiff(loadedNamespaces(), "base")
+            if( !length(pkgs) ) break
+            for (pkg in pkgs) {
+                try(unloadNamespace(pkg), silent = TRUE)
+            }
         }
     }
 }
