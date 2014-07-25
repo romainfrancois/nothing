c0dde596d568d9801a5aa98be4189e21e8aad6ab
Romain Francois <romain@r-enthusiasts.com>
2014-07-25 23:59:38 +0200

    using some suggestions from @wch. closes #1 closes #2

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,19 @@
+.onAttach <- function(libname, pkgname) {
+  # Don't unload self when being installed - that will result in install error
+  if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
+    try( unloadNamespace("nothing"), silent = TRUE )
+  }
+}
+   
 .onLoad <- function(libname, pkgname){
-    
-    repeat {
-        pkgs <- grep( "package:", search(), value = TRUE )
-        if( length(pkgs) == 1L ) break
-        pkgs <- sub( "package:", "", setdiff( pkgs, "package:base" ) )
-        for( pkg in pkgs ){
-            try( unloadNamespace(pkg), silent = TRUE )    
+    if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
+        pkgs <- setdiff(loadedNamespaces(), "base")  
+        while (length(pkgs) > 0) {
+          for (pkg in pkgs) {
+            tryCatch(unloadNamespace(pkg), error = function (e) invisible())
+          }
+        
+          pkgs <- setdiff(loadedNamespaces(), "base")
         }
     }
 }
