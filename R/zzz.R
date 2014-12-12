206f6a2e616abceaab7b97e90e1d48041384a61f
Gergely Daróczi <daroczig@rapporter.net>
2014-12-12 14:16:08 +0100

    fix #3: temporary divert stdout to /dev/null for increased user experience

--- R/zzz.R
+++ R/zzz.R
@@ -2,22 +2,37 @@
     # Don't unload self when being installed - that will result in install error
     suppressMessages(
         if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
-            try( unloadNamespace("nothing"), silent = TRUE )
+            try(unloadNamespace("nothing"), silent = TRUE)
         }
     )
 }
-   
-.onLoad <- function(libname, pkgname){
+
+.onLoad <- function(libname, pkgname) {
+
+    ## revert stdout and close temporary connection
+    on.exit({
+        sink()
+        close(con)
+    })
+
+    ## create a temporary connection where we can revert stdout
+    stdout <- vector('character')
+    con    <- textConnection('stdout', 'wr', local = TRUE)
+
+    ## suppress stdout
+    sink(con)
+
+    ## do actual stuff
     suppressMessages(
         if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
             repeat{
                 pkgs <- setdiff(loadedNamespaces(), "base")
-                if( !length(pkgs) ) break
+                if (!length(pkgs)) break
                 for (pkg in pkgs) {
                     try(unloadNamespace(pkg), silent = TRUE)
                 }
             }
         }
     )
-}
 
+}
